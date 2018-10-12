439b2737ff78a604db70009148fa79939a23d997
Sahib Jan <s_jan001@geoinformatik.geo.uni-muenster.de>
2018-10-12 14:54:24 +0200

    changes on 12-10-2018: to get id of maps in the webconnectior

---
+++ settings.py
@@ -0,0 +1,16 @@
+#settings.py
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 15:51:54 2018
+
+@author: s_jan001
+"""
+
+import os
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
+DIR_QCNS = "./output"
+DIR_DATA = "./data"
+metricMapID = None
+sketchMapID = None
